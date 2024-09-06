d5275624eb58abd3766690ec2d461d6e93779267
Trenton Ropp <trentonropp@Trenty-Pro.local>
2024-09-05 22:05:38 -0400

    added authorization function attached to button on frontend

---
+++ src/services/authorization.ts
@@ -0,0 +1,86 @@
+/* 
+The following is based on the Authorization Code with PKCE Flow provided 
+in Spotify's official API documentation here 
+https://developer.spotify.com/documentation/web-api/tutorials/code-pkce-flow 
+*/
+
+const CLIENT_ID = '67b236c90a274514a1668053e1da14c3';
+const REDIRECT_URI = 'http://localhost:5173';
+
+export const initRedirect = async () => {
+const generateRandomString = (length: number) => {
+    const possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    const values = crypto.getRandomValues(new Uint8Array(length));
+    return values.reduce((acc, x) => acc + possible[x % possible.length], "");
+}
+  
+const codeVerifier  = generateRandomString(64);
+
+//hash random string using sha-256, return array buffer object
+const sha256 = async (plain: string) => {
+    const encoder = new TextEncoder()
+    const data = encoder.encode(plain)
+    return window.crypto.subtle.digest('SHA-256', data)
+}
+
+//encode hashed string into base64 for GET request that needs simple text
+const base64encode = (input: ArrayBuffer) => {
+    return btoa(String.fromCharCode(...new Uint8Array(input)))
+        .replace(/=/g, '')
+        .replace(/\+/g, '-')
+        .replace(/\//g, '_');
+}
+
+const hashed = await sha256(codeVerifier);
+const codeChallenge = base64encode(hashed);
+
+
+    
+    
+
+    const scope = 'user-read-private user-read-email';
+    const authUrl = new URL("https://accounts.spotify.com/authorize")
+
+    // generated in the previous step
+    window.localStorage.setItem('code_verifier', codeVerifier);
+
+    const params =  {
+        response_type: 'code',
+        client_id: CLIENT_ID,
+        scope,
+        code_challenge_method: 'S256',
+        code_challenge: codeChallenge,
+        redirect_uri: REDIRECT_URI,
+    }
+
+    authUrl.search = new URLSearchParams(params).toString();
+    window.location.href = authUrl.toString();
+
+
+}
+
+const getToken = async (code: string) => {
+
+  // stored in the previous step
+  let codeVerifier = localStorage.getItem('code_verifier');
+
+  const payload = {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/x-www-form-urlencoded',
+    },
+    body: new URLSearchParams({
+      client_id: CLIENT_ID,
+      grant_type: 'authorization_code',
+      code,
+      redirect_uri: REDIRECT_URI,
+      //nullish coallescing--set code verifier to empty string if it returns null
+      code_verifier: codeVerifier ?? '',
+    }),
+  }
+
+  const body = await fetch("https://accounts.spotify.com/api/token", payload);
+  const response = await body.json();
+
+  localStorage.setItem('access_token', response.access_token);
+}
