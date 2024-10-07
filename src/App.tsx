c28f6e899e027021d200a9b4b19ad03efdb96310
Tanner Ropp <tanner.ropp@ufl.edu>
2024-10-07 19:31:38 -0400

    remove unnecessary import

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,21 @@
+import { useEffect, useState } from "react";
+import "./App.css";
+import { initRedirect, getToken } from "./services/authorization";
 
-import { useEffect, useState } from 'react'
-import './App.css'
-import { initRedirect, getToken } from './services/authorization'
-import ErrorPage from "./error-page";
 function App() {
-  const [isAuth, setIsAuth] = useState(false)
+  const [isAuth, setIsAuth] = useState(false);
   const urlParams = new URLSearchParams(window.location.search);
-  let code = urlParams.get('code');
+  let code = urlParams.get("code");
 
   useEffect(() => {
     if (!isAuth) {
       if (code) {
-        getToken(code).then(() => setIsAuth(true))
+        getToken(code).then(() => setIsAuth(true));
       }
     }
-  }, [isAuth])
+  }, [isAuth]);
 
-
-
-  
-  
-  return (
-    <button onClick={initRedirect}>Authorize</button>
-  )
+  return <button onClick={initRedirect}>Authorize</button>;
 }
 
-export default App
+export default App;
