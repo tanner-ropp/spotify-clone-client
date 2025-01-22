061a6e82b6b490cbbabda6bab2bd8a87862485f8
tanner-ropp <44204742+tanner-ropp@users.noreply.github.com>
2025-01-21 23:03:43 -0500

    Revert "Babel install"

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,28 @@
+import { useEffect, useState } from "react";
 import "./App.css";
-import { getIsAuthenticated } from "./services/authorization";
-import { UserPage } from "./user";
-import { GuestPage } from "./guest";
+import { initRedirect, getToken } from "./services/authorization";
+import styled from "styled-components";
 
-const App = () => {
-  return getIsAuthenticated() ? <UserPage /> : <GuestPage />;
-};
+function App() {
+  const [isAuth, setIsAuth] = useState(false);
+  const urlParams = new URLSearchParams(window.location.search);
+  let code = urlParams.get("code");
+
+  useEffect(() => {
+    if (!isAuth) {
+      if (code) {
+        getToken(code).then(() => setIsAuth(true));
+      }
+    }
+  }, [isAuth]);
+
+  return <AuthButton onClick={initRedirect}>Authorize</AuthButton>;
+}
+
+const AuthButton = styled.button`
+  background-color: ${({ theme }) => theme.green};
+  height: 3rem;
+  border-radius: 1.5rem;
+`;
 
 export default App;
